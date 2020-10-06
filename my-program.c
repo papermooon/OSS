7baae3f06305db4abe6ad68d8558a11fa2204b50
papermooon <63181559+papermooon@users.noreply.github.com>
2020-10-07 01:14:16 +0800

    Update my-program.c

--- my-program.c
+++ my-program.c
@@ -4,8 +4,12 @@
  char token[100];
  char standin[200];
  
- int main()
- {
- 	scanf("%s",standin);
- 	printf("%s",standin);
- };
+int main(int argc,char *argv[]) {
+    int count;
+    printf("The command line has %d arguments :\n",argc-1);
+    for (count = 1; count < argc; ++count) {
+        printf("%d: %s\n",count,argv[count]);
+    }
+
+    return 0;
+}
